62ba547a2f2113eb632dd8710588aca73b95e265
Nikita Kojemyaka <akitin98@gmail.com>
2022-02-17 10:15:04 +0000

    apache2 cgi version

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 #Current time with word Hello
 import datetime
-
+import os
 def do_magic():
  now = datetime.datetime.now()
  return "Hello! {0}".format(now)
 
 if __name__ == "__main__":
+#if 'REQUEST_URI' in os.environ:
  print("Content-type: text/html\n\n")
+# print(os.environ)
  print (do_magic())
