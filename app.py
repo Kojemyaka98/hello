b4559d3507d6e414385fb062aa3962306b718a49
Nikita Kojemyaka <akitin98@gmail.com>
2022-02-17 10:40:07 +0000

    uwsgi version

--- app.py
+++ app.py
@@ -2,16 +2,16 @@
 #Current time with word Hello
 import datetime
 import os
+
 def do_magic():
  now = datetime.datetime.now()
  return "Hello! {0}".format(now)
 
 def application(env, start_response):
  start_response('200 OK', [('Content-Type', 'text/html')])
-return [do_magic()]
+ return [do_magic().encode()]
 
 if __name__ == "__main__":
-#if 'REQUEST_URI' in os.environ:
- print("Content-type: text/html\n\n")
-# print(os.environ)
+ if 'REQUEST_URI' in os.environ:
+  print("Content-type: text/html\n\n")
  print (do_magic())
